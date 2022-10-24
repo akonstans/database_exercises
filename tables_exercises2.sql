9d23a26316c826ddd1cb5387bdcbdbb0b41f5cda
Andrew Konstans <andrew.konstans10@gmail.com>
2022-10-24 13:48:51 -0500

    edits

--- tables_exercises2.sql
+++ tables_exercises2.sql
@@ -1,18 +1,19 @@
+USE employees;
 SHOW Tables;
 SELECT * FROM employees;
 /* Which table(s) do you think contain a numeric type column?
-emp_no */
+all of them 
+*/
 /* Which table(s) do you think contain a string type column?
-first_name and last_name */
-SHOW CREATE TABLE employees;
+all of them except salaries 
+*/
 /* Which table(s) do you think contain a date type column?
-birth_date and hire_date */
+employees table 
+*/
 /* What is the relationship between the employees and the departments tables?
 The employee number is linked to the dep num in the table dep manager
 */
-SELECT * FROM departments;
-SELECT * FROM employees;
-SELECT * FROM dept_manager;
+
 SHOW CREATE TABLE dept_manager;
 /* CREATE TABLE `dept_manager` (
   `emp_no` int NOT NULL,
@@ -23,4 +24,6 @@ SHOW CREATE TABLE dept_manager;
   KEY `dept_no` (`dept_no`),
   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 */
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 
+*/
+
