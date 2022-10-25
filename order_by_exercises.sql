d9ad0495be957ed342d67bd748d974c56322ec0f
Andrew Konstans <andrew.konstans10@gmail.com>
2022-10-25 12:07:01 -0500

    Limit Exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,60 +1,19 @@
-USE `employees`
-SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name
--- There are 709 past or current employees with those names
--- First Irena R Last Vidya S
-SELECT * FROM employees
-WHERE first_name = 'Irena'
-OR first_name = 'Vidya'
-OR first_name = 'Maya'
-ORDER BY first_name, last_name
--- There are still 709
--- First Irena A Last Vidya Z
-
-SELECT * FROM employees
-WHERE first_name = 'Irena'
-OR first_name = 'Vidya'
-OR first_name = 'Maya'
-ORDER BY last_name, first_name
--- First Irena A Last Maya Z
-
-
-SELECT COUNT(*) FROM employees
-WHERE last_name LIKE 'e%' 
--- There are 7330 
-
-SELECT * FROM employees
-WHERE last_name LIKE 'e%'
-AND last_name LIKE '%e'
-ORDER BY hire_date DESC
--- 899 results, Teiji newest Sergei Oldest
-
-SELECT * FROM employees
-WHERE last_name LIKE 'e%'
-AND last_name LIKE '%e'
-ORDER BY emp_no
--- There are 899 of them
--- First is emp 10021 and last is emp 499648 Tad E
-
-SELECT COUNT(*) FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
--- There are 135214 of them
+SELECT DISTINCT title FROM titles ORDER BY title DESC
+-- Technique Leader, Staff, Senior Staff, Senior Engineer, Manager, Engineer, Assistant Engineer
 
 SELECT * FROM employees
 WHERE birth_date LIKE '%-12-25'
-ORDER BY birth_date ASC, hire_date DESC
--- There are 842 born on Christmas
--- Woody oldest hire youngest, Khun newest hire oldest
-SELECT COUNT(*) FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25'
--- There are 362 hired in 90's and born on Christmas
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY hire_date
+LIMIT 5
+-- Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup
 
-SELECT COUNT(*) FROM employees
-WHERE last_name LIKE '%q%'
--- There are 1873 with the letter Q
+SELECT * FROM employees
+WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY hire_date
+LIMIT 5 OFFSET 45
+
+-- LIMIT will ensure each 'page' is 5 names long while OFFSET can set the 'page' number by skipping through a number of names until the desired 'page' is reached
 
 
-SELECT COUNT(*) FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
--- There are 547 with q but not qu
